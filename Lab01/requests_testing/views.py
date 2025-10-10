1a843f94064a5016ce9c43af44e6f64594632622
Bobby Yu <Work.BobbyYu@gmail.com>
2025-10-10 15:23:05 +0100

    Lab 01 - 4

--- Lab01/requests_testing/views.py
+++ Lab01/requests_testing/views.py
@@ -1,7 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 # Create your views here.
-def hello_world(request): #This is a method called "hello_world" which takes a single parameter called "request".
-    message_text = request.GET.get('message', 'Hello world!') #This gets a "message" parameter. If it is not there, defaults to "Hello world!".
-    return HttpResponse("Hello world!") #The "HttpResponse" class returns the message "Hello world!".
+# # This is a method called "hello_world" which takes a single parameter called "request".
+# def hello_world(request):
+#     # This gets a "message" parameter. If it is not there, defaults to "Hello world!".
+#     message_text = request.GET.get('message', 'Hello world!')
+#     # The "HttpResponse" class returns the message "Hello world!".
+#     return HttpResponse(message_text)
+
+def hello_world(request):
+    # Checks for custom headers.
+    header_message = request.headers.get('MY-APPLICATION-MESSAGE')
+
+    if header_message:
+        message_text = header_message
+    else:
+        # If there is no header, check for the GET parameter.
+        message_text = request.GET.get('message', 'Hello world!')
+
+    return HttpResponse(message_text)
