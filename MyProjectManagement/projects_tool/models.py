c3aefa93e38cd11fc74a5d66bf8751b2da3c297d
Bobby Yu <Work.BobbyYu@gmail.com>
2025-10-20 20:52:27 +0100

    MyProjectManagement - 3

--- MyProjectManagement/projects_tool/models.py
+++ MyProjectManagement/projects_tool/models.py
@@ -1,7 +1,15 @@
 from django.db import models
 # Imports the slug function. A slug is a string which typically contains only letters, numbers, underscores, or hyphens, making it safe to use in a URL path.
 from django.utils.text import slugify
-from django.core.validators import RegexValidator
+from django.core.exceptions import ValidationError
+from django.core.validators import RegexValidator, MinValueValidator, MaxValueValidator
+
+def validate_divisible_by_5(value):
+    # Checks if the value is divisible by 5.
+    if value % 5 != 0:
+        raise ValidationError(f'{value} is not divisible by 5.')
+
+
 
 # Create your models here.
 class Project(models.Model): # We inherit from the "models.Model" base class to create a Django model.
@@ -91,3 +99,36 @@ class List(models.Model):
 
     def __str__(self):
         return self.title
+
+
+
+class Task(models.Model):
+    # A single task or card that exists within a list.
+    class Priority(models.TextChoices):
+        # First '' like 'HI' is what is stored in the database. Second '' like 'High' is the human-readable name in the forms and admin panel.
+        HIGH = 'HI', 'High'
+        MEDIUM = 'ME', 'Medium'
+        LOW = 'LO', 'Low'
+
+    # "task_no" is the primary key. "primary_key=True" tells Django to use this field as the unique identifier instead of the default "id" field.
+    task_no = models.AutoField(primary_key=True)
+    # This links each task to a "List". If a list is deleted, all of its tasks are deleted ("CASCADE").
+    list = models.ForeignKey(List, on_delete=models.CASCADE, related_name='tasks')
+    title = models.CharField(max_length=64)
+    description = models.CharField(max_length=512, blank=True, null=True)
+    # Priority for new tasks are set as medium as default.
+    priority = models.CharField(max_length=2, choices=Priority.choices, default=Priority.MEDIUM)
+    story_points = models.IntegerField(
+        validators=[
+            # Makes sure the number is between 0 and 100.
+            MinValueValidator(0),
+            MaxValueValidator(100),
+            # Custom function for divisible by 5.
+            validate_divisible_by_5
+        ]
+    )
+    # This makes a many-to-many relationship. Blank allows a task to be created without any labels.
+    labels = models.ManyToManyField(Label, related_name='tasks', blank=True)
+
+    def __str__(self):
+        return f"#{self.task_no}: {self.title}"
