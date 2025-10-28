e9485536e660d48423f75bf706bbc68beb51c53a
Bobby Yu <Work.BobbyYu@gmail.com>
2025-10-28 18:48:16 +0000

    MyProjectManagement - 8

--- MyProjectManagement/projects_tool/admin.py
+++ MyProjectManagement/projects_tool/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Project, Board, List, Task, Label
 
 # Register your models here.
+admin.site.register(Project)
+admin.site.register(Board)
+admin.site.register(List)
+admin.site.register(Task)
+admin.site.register(Label)
